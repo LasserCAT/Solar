8f3de631a503376b2d0f45cb24132b60385ab18c
Unknown <mcpkmart@hotmail.nl>
2018-01-31 12:53:11 +0100

    Use UUID's instead of strings

--- src/main/java/com/mart/solar/common/world/data/InteractedWithAltarData.java
+++ src/main/java/com/mart/solar/common/world/data/InteractedWithAltarData.java
@@ -2,20 +2,21 @@ package com.mart.solar.common.world.data;
 
 import com.mart.solar.Solar;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.world.World;
 import net.minecraft.world.storage.MapStorage;
 import net.minecraft.world.storage.WorldSavedData;
+import net.minecraftforge.common.util.Constants;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
 
-public class InteractedWithAltarData extends WorldSavedData{
+public class InteractedWithAltarData extends WorldSavedData {
 
     private static final String IDENTIFIER = Solar.MODID + "interactedWithAltarList";
-    private String playerUUIDs = "";
 
-    private List<String> playerUUIDList = new ArrayList<>();
+    private List<UUID> playerUUIDSList = new ArrayList<>();
 
     private InteractedWithAltarData() {
         super(IDENTIFIER);
@@ -23,41 +24,43 @@ public class InteractedWithAltarData extends WorldSavedData{
 
     @Override
     public void readFromNBT(NBTTagCompound nbt) {
-        this.playerUUIDs = nbt.getString("playerUUIDS");
-        readyList();
+        NBTTagList tagList = nbt.getTagList("playerUUIDS", Constants.NBT.TAG_COMPOUND);
+        this.playerUUIDSList.clear();
+        for(int i = 0; i < tagList.tagCount(); i++){
+            NBTTagCompound tag = tagList.getCompoundTagAt(i);
+            UUID uuid = tag.getUniqueId("uuid");
+            this.playerUUIDSList.add(uuid);
+        }
     }
 
     @Override
     public NBTTagCompound writeToNBT(NBTTagCompound nbt) {
-        nbt.setString("playerUUIDS", playerUUIDs);
+        NBTTagList tagList = new NBTTagList();
+        for(UUID uuid : playerUUIDSList){
+            NBTTagCompound tag = new NBTTagCompound();
+            tag.setUniqueId("uuid", uuid);
+            tagList.appendTag(tag);
+        }
+
+        nbt.setTag("playerUUIDS", tagList);
         return null;
     }
 
-    private void readyList(){
-        String[] stringsArray = playerUUIDs.split(", ");
-        this.playerUUIDList.clear();
-
-        Collections.addAll(this.playerUUIDList, stringsArray);
-    }
-
-    public void addUUID(String UUID){
-        if(this.playerUUIDs.equalsIgnoreCase("")){
-            this.playerUUIDs = UUID;
-        }
-        else{
-            this.playerUUIDs = this.playerUUIDs + ", " + UUID;
+    public void addUUID(UUID uuid) {
+        if(!this.playerUUIDSList.contains(uuid)){
+            this.playerUUIDSList.add(uuid);
             markDirty();
         }
     }
 
-    public List<String> getUUIDList(){
-        return this.playerUUIDList;
+    public List<UUID> getUUIDList() {
+        return this.playerUUIDSList;
     }
 
-    public static InteractedWithAltarData getData(World world){
+    public static InteractedWithAltarData getData(World world) {
         MapStorage storage = world.getMapStorage();
         InteractedWithAltarData data = (InteractedWithAltarData) storage.getOrLoadData(InteractedWithAltarData.class, IDENTIFIER);
-        if(data == null){
+        if (data == null) {
             data = new InteractedWithAltarData();
             storage.setData(IDENTIFIER, data);
         }
