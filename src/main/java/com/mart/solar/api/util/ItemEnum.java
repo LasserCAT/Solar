0fb0515506b261fc0d1ba2badd8b3f30d798499b
Unknown <mcpkmart@hotmail.nl>
2018-02-04 20:49:44 +0100

    SubItemsShowUpInCreaativeTaab

--- src/main/java/com/mart/solar/api/util/ItemEnum.java
+++ src/main/java/com/mart/solar/api/util/ItemEnum.java
@@ -5,6 +5,7 @@ import com.mart.solar.Solar;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -27,10 +28,16 @@ public class ItemEnum<T extends Enum<T>> extends Item {
         return super.getUnlocalizedName(stack) + "." + getItemType(stack).toString().toLowerCase(Locale.ENGLISH);
     }
 
+    @Override
+    public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> items) {
+        for (T type : types) {
+            items.add(new ItemStack(this, 1, type.ordinal()));
+        }
+    }
+
     @SideOnly(Side.CLIENT)
     public void getSubItems(Item itemIn, CreativeTabs tab, List<ItemStack> subItems) {
-        for (T type : types)
-            subItems.add(new ItemStack(this, 1, type.ordinal()));
+
     }
 
     public T getItemType(ItemStack stack) {
