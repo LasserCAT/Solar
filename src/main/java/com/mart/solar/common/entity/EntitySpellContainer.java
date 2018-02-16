81054fd95e4f9e67d1357154a977579427b19ac3
Unknown <mcpkmart@hotmail.nl>
2018-02-16 15:24:55 +0100

    Forgot this

--- src/main/java/com/mart/solar/common/entity/EntitySpellContainer.java
+++ src/main/java/com/mart/solar/common/entity/EntitySpellContainer.java
@@ -3,11 +3,14 @@ package com.mart.solar.common.entity;
 import com.mart.solar.Solar;
 import com.mart.solar.api.interfaces.IPlaceAbleSpell;
 import com.mart.solar.api.spell.Spell;
+import com.mart.solar.api.spell.SpellManager;
+import com.mart.solar.common.items.ItemBase;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 
+import java.util.Optional;
 import java.util.logging.Level;
 
 public class EntitySpellContainer extends Entity{
@@ -27,6 +30,11 @@ public class EntitySpellContainer extends Entity{
         this.setPosition(player.getPosition().getX(), player.getPosition().getY(), player.getPosition().getZ());
 
         this.iPlaceAbleSpell = iPlaceAbleSpell;
+
+        if(!(this.iPlaceAbleSpell instanceof Spell)){
+            System.out.println("[EntitySpellContainer.java] Removing iPlaceAbleSpell entity. iPlaceAbleSpell was not an instance of a Spell.class");
+            getEntityWorld().removeEntity(this);
+        }
     }
 
     @Override
@@ -35,23 +43,28 @@ public class EntitySpellContainer extends Entity{
 
     @Override
     protected void readEntityFromNBT(NBTTagCompound compound) {
+        Optional<Spell> spell = SpellManager.getSpells().stream()
+                .filter(s -> s.getSpellRegistryName().equals(Spell.getSpellHandleFromNBT(compound))).findFirst();
+
+        if(spell.isPresent()){
+            Spell copySpell = spell.get().getNewInstance();
+            copySpell.getDataFromNBT(compound);
+
+            this.iPlaceAbleSpell = (IPlaceAbleSpell) copySpell;
+        }
+
 
     }
 
     @Override
     protected void writeEntityToNBT(NBTTagCompound compound) {
-
+        Spell spell = (Spell) this.iPlaceAbleSpell;
+        spell.saveSpellHandleToNBT(compound);
+        spell.saveDataToNBT(compound);
     }
 
     @Override
     public void onEntityUpdate() {
-        if(!(this.iPlaceAbleSpell instanceof Spell)){
-            //Solar.logger.log(Level.WARNING, "[EntitySpellContainer.java] Removing iPlaceAbleSpell entity. iPlaceAbleSpell was not an instance of a Spell.class");
-            System.out.println("[EntitySpellContainer.java] Removing iPlaceAbleSpell entity. iPlaceAbleSpell was not an instance of a Spell.class");
-            getEntityWorld().removeEntity(this);
-            return;
-        }
-
         Spell spell = (Spell) this.iPlaceAbleSpell;
 
         if(spell.getLifeSpan() <= this.lifeTime){
