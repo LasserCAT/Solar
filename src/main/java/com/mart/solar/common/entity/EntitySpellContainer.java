83d9d6b206f7a6ea5a479bd837d890aa6aa4df57
Unknown <mcpkmart@hotmail.nl>
2018-02-16 15:34:04 +0100

    Going to serialize spell

--- src/main/java/com/mart/solar/common/entity/EntitySpellContainer.java
+++ src/main/java/com/mart/solar/common/entity/EntitySpellContainer.java
@@ -1,21 +1,18 @@
 package com.mart.solar.common.entity;
 
-import com.mart.solar.Solar;
 import com.mart.solar.api.interfaces.IPlaceAbleSpell;
 import com.mart.solar.api.spell.Spell;
 import com.mart.solar.api.spell.SpellManager;
-import com.mart.solar.common.items.ItemBase;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 
 import java.util.Optional;
-import java.util.logging.Level;
 
 public class EntitySpellContainer extends Entity{
 
-    private IPlaceAbleSpell iPlaceAbleSpell;
+    private Spell spell;
 
     private int lifeTime;
 
@@ -23,25 +20,23 @@ public class EntitySpellContainer extends Entity{
         super(worldIn);
     }
 
-    public EntitySpellContainer(World worldIn, EntityPlayer player, IPlaceAbleSpell iPlaceAbleSpell) {
+    public EntitySpellContainer(World worldIn, EntityPlayer player, Spell iPlaceAbleSpell) {
         super(worldIn);
 
         this.setWorld(worldIn);
         this.setPosition(player.getPosition().getX(), player.getPosition().getY(), player.getPosition().getZ());
 
-        this.iPlaceAbleSpell = iPlaceAbleSpell;
+        this.spell = iPlaceAbleSpell;
     }
 
     @Override
     protected void entityInit() {
-        if(!(iPlaceAbleSpell instanceof Spell)){
-            System.out.println("[EntitySpellContainer.java] Removing iPlaceAbleSpell entity. iPlaceAbleSpell was not an instance of a Spell.class");
-            getEntityWorld().removeEntity(this);
-        }
     }
 
     @Override
     protected void readEntityFromNBT(NBTTagCompound compound) {
+        this.lifeTime = compound.getInteger("lifeTime");
+
         Optional<Spell> spell = SpellManager.getSpells().stream()
                 .filter(s -> s.getSpellRegistryName().equals(Spell.getSpellHandleFromNBT(compound))).findFirst();
 
@@ -49,7 +44,7 @@ public class EntitySpellContainer extends Entity{
             Spell copySpell = spell.get().getNewInstance();
             copySpell.getDataFromNBT(compound);
 
-            this.iPlaceAbleSpell = (IPlaceAbleSpell) copySpell;
+            this.spell = copySpell;
         }
 
 
@@ -57,20 +52,25 @@ public class EntitySpellContainer extends Entity{
 
     @Override
     protected void writeEntityToNBT(NBTTagCompound compound) {
-        Spell spell = (Spell) this.iPlaceAbleSpell;
+        compound.setInteger("lifeTime", this.lifeTime);
+
+        Spell spell = this.spell;
         spell.saveSpellHandleToNBT(compound);
         spell.saveDataToNBT(compound);
     }
 
     @Override
     public void onEntityUpdate() {
-        Spell spell = (Spell) this.iPlaceAbleSpell;
+        if(!(spell instanceof IPlaceAbleSpell)){
+            System.out.println("[EntitySpellContainer.java] Removing spell entity. spell was not an instance of a Spell.class");
+            getEntityWorld().removeEntity(this);
+        }
 
         if(spell.getLifeSpan() <= this.lifeTime){
             getEntityWorld().removeEntity(this);
         }
-
-        this.iPlaceAbleSpell.tick(this.getEntityWorld(), this.getPosition(), this.lifeTime);
+        IPlaceAbleSpell placeAbleSpell = (IPlaceAbleSpell) this.spell;
+        placeAbleSpell.tick(this.getEntityWorld(), this.getPosition(), this.lifeTime);
 
         this.lifeTime++;
     }
