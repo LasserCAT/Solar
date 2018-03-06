93e83f06924c0c0fab9ee6a31f06b8393aa891f3
Unknown <mcpkmart@hotmail.nl>
2018-03-06 19:48:58 +0100

    Version 0.0.1.4

--- src/main/java/com/mart/solar/Solar.java
+++ src/main/java/com/mart/solar/Solar.java
@@ -33,7 +33,7 @@ public class Solar
 
     public static final String MODID = "solar";
     public static final String NAME = "Solar";
-    public static final String VERSION = "0.0.1.3";
+    public static final String VERSION = "0.0.1.4";
 
     public static Logger logger;
 
